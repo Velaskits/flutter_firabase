5093f5db87879371755ad1aaf9545308c7481502
Velaskits <velascomateoalex@gmail.com>
2024-02-23 19:27:35 +0100

    UI Login feta

--- lib/paginas/pagina_login.dart
+++ lib/paginas/pagina_login.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firabase/components/boto_auth.dart';
 import 'package:flutter_firabase/components/textfield_auth.dart';
 
 class PaginaLogin extends StatefulWidget {
-
   const PaginaLogin({super.key});
 
   @override
@@ -10,7 +10,6 @@ class PaginaLogin extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<PaginaLogin> {
-
   final TextEditingController controllerEmail = TextEditingController();
   final TextEditingController controllerPassword = TextEditingController();
 
@@ -22,13 +21,17 @@ class _MyWidgetState extends State<PaginaLogin> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-        
             // Logo.
             const Icon(
               Icons.fireplace,
               size: 120,
               color: Color.fromARGB(255, 255, 240, 218),
             ),
+
+            const SizedBox(
+              height: 25,
+            ),
+
             // Frase.
             const Text(
               "Benvinbingut/a de nou.",
@@ -39,52 +42,97 @@ class _MyWidgetState extends State<PaginaLogin> {
               ),
             ),
 
+            const SizedBox(
+              height: 25,
+            ),
+
             // Text divisori.
-            const Row(
-              children: [
-                Expanded(
-                  child: Divider(
-                    thickness: 1,
-                    color: Color.fromARGB(255, 255, 240, 218),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 4,),
-                  child: Text(
-                    "Fes login",
-                    style: TextStyle(
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
                       color: Color.fromARGB(255, 255, 240, 218),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Divider(
-                    thickness: 1,
-                    color: Color.fromARGB(255, 255, 240, 218),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 4,
+                    ),
+                    child: Text(
+                      "Fes login",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 240, 218),
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Color.fromARGB(255, 255, 240, 218),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(
+              height: 10,
             ),
 
             // TextField Email.
             TextFieldAuth(
-              controller: controllerEmail, 
-              obscureText: false, 
+              controller: controllerEmail,
+              obscureText: false,
               hintText: "Email",
+            ),
+
+            const SizedBox(
+              height: 10,
             ),
 
             // TextField Password.
             TextFieldAuth(
-              controller: controllerPassword, 
-              obscureText: true, 
+              controller: controllerPassword,
+              obscureText: true,
               hintText: "Password",
-            )
+            ),
+
+            const SizedBox(
+              height: 10,
+            ),
 
             // No estas registrat/da?
-            
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 25,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Text("No ets membre?"),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Text(
+                      "Registra't",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, 
+                          color: Colors.blue),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 10,),
 
             // Botó login.
-
+            const BotoAuth(),
 
           ],
         ),
